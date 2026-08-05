28bbb42174f08d603c8c72387cd4bf5ea3f4ffb2
ishandutta2007 <ishandutta2007@gmail.com>
2026-08-06 05:05:37 +0530

    cleanup

--- JEE_Aspirants_vs_Acceptance.py
+++ JEE_Aspirants_vs_Acceptance.py
@@ -42,7 +42,7 @@ for i, txt in enumerate(aspirants):
 
 # Annotate Seats
 for i, txt in enumerate(seats):
-    ax1.annotate(f'{txt:,}', (years[i], seats[i]), textcoords="offset points", xytext=(0,-18), ha='center', fontsize=8, color='#2980b9', fontweight='bold', bbox=dict(boxstyle="round,pad=0.2", fc="white", ec='#3498db', alpha=0.85))
+    ax1.annotate(f'{txt:,}', (years[i], seats[i]), textcoords="offset points", xytext=(0,12), ha='center', fontsize=8, color='#2980b9', fontweight='bold', bbox=dict(boxstyle="round,pad=0.2", fc="white", ec='#3498db', alpha=0.85))
 
 # Annotate Acceptance Rate
 for i, txt in enumerate(acceptance_rate):
